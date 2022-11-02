4523b68b964c1ae8b6b7035646f9ada0f5320fd7
Antonio Sein Jagou <antonioseinjagou95@gmail.com>
2022-11-01 22:59:28 -0600

    Detail page created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home   from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+//The Route will let know to show detail or to bring back to home page with the same header
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+     <Router>
+        <Header/>
+        <Switch>
+          <Route path="/detail">
+            <Detail/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+     </Router>
     </div>
   );
 }
